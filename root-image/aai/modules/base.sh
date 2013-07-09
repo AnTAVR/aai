0a5dbdcbf7a4ed60193a0dca41ad9cd0cbfcc4bf
Anthony Lyappiev <archlinux@antavr.ru>
2013-07-09 22:49:11 +0300

    Убрал добавление в /etc/hosts имени хоста, потому что лишнее!

--- root-image/aai/modules/base.sh
+++ root-image/aai/modules/base.sh
@@ -731,8 +731,8 @@ s/^/#/;
 	echo "PRETTY_HOSTNAME=${HOSTNAME}" > "${NS_PATH}/etc/machine-info"
 
 # Добавляем в /etc/hosts имя хоста
-	msg_log "$(gettext 'Настраиваю') /etc/hosts"
-	echo -e "127.0.0.1\t${SET_HOSTNAME}\t${HOSTNAME}" >> "${NS_PATH}/etc/hosts"
+#	msg_log "$(gettext 'Настраиваю') /etc/hosts"
+#	echo -e "127.0.0.1\t${SET_HOSTNAME}\t${HOSTNAME}" >> "${NS_PATH}/etc/hosts"
 #-------------------------------------------------------------------------------
 
 
