1c7cbc3c5a1f514275bdb32d78f88c47d39ce12f
Anthony Lyappiev <archlinux@antavr.ru>
2015-05-01 01:43:21 +0300

    удалил systemd-readahead, больше их нет в systemd

--- airootfs/aai/modules/base_plus.sh
+++ airootfs/aai/modules/base_plus.sh
@@ -219,8 +219,6 @@ base_plus_dialog_service()
 	local HELP_TXT="\n$(gettext 'Включение сервисов')\n"
 
 	local ITEMS="${SERVICES}"
-	ITEMS+=" 'systemd-readahead-collect' '-' 'off'"
-	ITEMS+=" 'systemd-readahead-replay' '-' 'off'"
 
 	RETURN="$(dialog_checklist "${TITLE}" "${HELP_TXT}" "${ITEMS}" '--no-cancel')"
 
